52f08dc63a7d003b5c7a8881d2d60f3ab34d4902
kevin <zuohua.ma@gmail.com>
2022-02-27 12:17:58 +0800

    增加自定义框架及pms、uum

---
+++ src/main/java/me/henglee/dashboard/pro/freamwork/domain/BaseEntity.java
@@ -0,0 +1,9 @@
+package me.henglee.dashboard.pro.freamwork.domain;
+
+/**
+ * Created by Andy on 2022/2/27.
+ */
+public class BaseEntity implements java.io.Serializable
+{
+    private static final long serialVersionUID = 1L;
+}
